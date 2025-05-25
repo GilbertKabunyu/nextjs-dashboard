67c36792d9c87dd04e49e4d2101e1540ceda3d33
GilbertKabunyu <gkabunyuk15@gmail.com>
2025-05-25 18:45:12 +0200

    week 3 assignment

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+// /app/dashboard/customers/page.tsx
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+
 export default function Page() {
   return <p>Customers Page</p>;
 }
